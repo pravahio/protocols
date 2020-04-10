b677111c361d5bd021f5614a0848d6930e4cd1db
Abhishek Upperwal <a.upperwal@gmail.com>
2020-04-10 19:10:42 +0530

    renaming

--- compiled/py/pravah_protocols/HospitalCapacity_pb2.py
+++ compiled/py/pravah_protocols/HospitalCapacity_pb2.py
@@ -19,15 +19,15 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   package='health_hospital_capacity',
   syntax='proto3',
   serialized_options=b'\n\"io.pravah.health.hospital_capacity',
-  serialized_pb=b'\n\x1dhealth/HospitalCapacity.proto\x12\x18health_hospital_capacity\x1a\x10\x46\x65\x65\x64Header.proto\"a\n\x0b\x46\x65\x65\x64Message\x12\x1b\n\x06header\x18\x01 \x01(\x0b\x32\x0b.FeedHeader\x12\x35\n\thospitals\x18\x02 \x03(\x0b\x32\".health_hospital_capacity.Hospital\"\xe7\x01\n\x08Hospital\x12\x30\n\x06\x64\x65tail\x18\x01 \x01(\x0b\x32 .health_hospital_capacity.Detail\x12@\n\x0einfrastructure\x18\x02 \x01(\x0b\x32(.health_hospital_capacity.Infrastructure\x12.\n\x05staff\x18\x03 \x01(\x0b\x32\x1f.health_hospital_capacity.Staff\x12\x37\n\nequipments\x18\x04 \x03(\x0b\x32#.health_hospital_capacity.Equipment\"\xc2\x02\n\x06\x44\x65tail\x12\x0c\n\x04uuid\x18\x01 \x01(\t\x12\x0c\n\x04name\x18\x02 \x01(\t\x12\x34\n\x08location\x18\x03 \x01(\x0b\x32\".health_hospital_capacity.Location\x12=\n\townership\x18\x04 \x01(\x0e\x32*.health_hospital_capacity.Detail.Ownership\x12?\n\nspeciality\x18\x05 \x01(\x0e\x32+.health_hospital_capacity.Detail.Speciality\"1\n\tOwnership\x12\x0b\n\x07UNKNOWN\x10\x00\x12\n\n\x06PUBLIC\x10\x01\x12\x0b\n\x07PRIVATE\x10\x02\"3\n\nSpeciality\x12\x0b\n\x07GENERAL\x10\x00\x12\r\n\tMATERNITY\x10\x01\x12\t\n\x05\x43OVID\x10\x04\"@\n\x08Location\x12\x0f\n\x07\x61\x64\x64ress\x18\x01 \x01(\t\x12\x10\n\x08latitude\x18\x02 \x01(\x02\x12\x11\n\tlongitude\x18\x03 \x01(\x02\"?\n\x0eInfrastructure\x12-\n\x05wards\x18\x01 \x03(\x0b\x32\x1e.health_hospital_capacity.Ward\"\xc5\x01\n\x04Ward\x12\x0c\n\x04uuid\x18\x01 \x01(\t\x12\x32\n\x05types\x18\x02 \x03(\x0e\x32#.health_hospital_capacity.Ward.Type\x12\x14\n\x0cnumberOfBeds\x18\x03 \x01(\r\x12\x1c\n\x14numberOfOccupiedBeds\x18\x04 \x01(\r\"G\n\x04Type\x12\x0b\n\x07UNKNOWN\x10\x00\x12\x07\n\x03ICU\x10\x01\x12\r\n\tEMERGENCY\x10\x02\x12\x0b\n\x07GENERAL\x10\x03\x12\r\n\tISOLATION\x10\x04\"a\n\x05Staff\x12\x14\n\x0ctotalDoctors\x18\x01 \x01(\r\x12\x13\n\x0btotalNurses\x18\x02 \x01(\r\x12\x19\n\x11totalSupportStaff\x18\x03 \x01(\r\x12\x12\n\notherStaff\x18\x04 \x01(\r\"\xfb\x01\n\tEquipment\x12\x0c\n\x04uuid\x18\x01 \x01(\t\x12\x36\n\x04type\x18\x02 \x01(\x0e\x32(.health_hospital_capacity.Equipment.Type\x12\x15\n\rquantityTotal\x18\x03 \x01(\r\x12\x15\n\rquantityInUse\x18\x04 \x01(\r\x12\x1d\n\x15\x65xtraQuantityRequired\x18\x05 \x01(\r\x12\x0c\n\x04tags\x18\x06 \x03(\t\"M\n\x04Type\x12\x0b\n\x07UNKNOWN\x10\x00\x12\x0b\n\x07N95MASK\x10\x01\x12\x0e\n\nVENTILATOR\x10\x02\x12\n\n\x06GLOVES\x10\x03\x12\x0f\n\x0b\x46\x41\x43\x45_SHIELD\x10\x04\x42$\n\"io.pravah.health.hospital_capacityb\x06proto3'
+  serialized_pb=b'\n\x1dhealth/HospitalCapacity.proto\x12\x18health_hospital_capacity\x1a\x10\x46\x65\x65\x64Header.proto\"a\n\x0b\x46\x65\x65\x64Message\x12\x1b\n\x06header\x18\x01 \x01(\x0b\x32\x0b.FeedHeader\x12\x35\n\thospitals\x18\x02 \x03(\x0b\x32\".health_hospital_capacity.Hospital\"\xe9\x01\n\x08Hospital\x12\x32\n\x07\x64\x65tails\x18\x01 \x01(\x0b\x32!.health_hospital_capacity.Details\x12@\n\x0einfrastructure\x18\x02 \x01(\x0b\x32(.health_hospital_capacity.Infrastructure\x12.\n\x05staff\x18\x03 \x01(\x0b\x32\x1f.health_hospital_capacity.Staff\x12\x37\n\nequipments\x18\x04 \x03(\x0b\x32#.health_hospital_capacity.Equipment\"\xc5\x02\n\x07\x44\x65tails\x12\x0c\n\x04uuid\x18\x01 \x01(\t\x12\x0c\n\x04name\x18\x02 \x01(\t\x12\x34\n\x08location\x18\x03 \x01(\x0b\x32\".health_hospital_capacity.Location\x12>\n\townership\x18\x04 \x01(\x0e\x32+.health_hospital_capacity.Details.Ownership\x12@\n\nspeciality\x18\x05 \x01(\x0e\x32,.health_hospital_capacity.Details.Speciality\"1\n\tOwnership\x12\x0b\n\x07UNKNOWN\x10\x00\x12\n\n\x06PUBLIC\x10\x01\x12\x0b\n\x07PRIVATE\x10\x02\"3\n\nSpeciality\x12\x0b\n\x07GENERAL\x10\x00\x12\r\n\tMATERNITY\x10\x01\x12\t\n\x05\x43OVID\x10\x04\"@\n\x08Location\x12\x0f\n\x07\x61\x64\x64ress\x18\x01 \x01(\t\x12\x10\n\x08latitude\x18\x02 \x01(\x02\x12\x11\n\tlongitude\x18\x03 \x01(\x02\"?\n\x0eInfrastructure\x12-\n\x05wards\x18\x01 \x03(\x0b\x32\x1e.health_hospital_capacity.Ward\"\xe0\x01\n\x04Ward\x12\x0c\n\x04uuid\x18\x01 \x01(\t\x12\x32\n\x05types\x18\x02 \x03(\x0e\x32#.health_hospital_capacity.Ward.Type\x12\x14\n\x0cnumberOfBeds\x18\x03 \x01(\r\x12\x1c\n\x14numberOfOccupiedBeds\x18\x04 \x01(\r\x12\x19\n\x11\x65xtraBedsRequired\x18\x05 \x01(\r\"G\n\x04Type\x12\x0b\n\x07UNKNOWN\x10\x00\x12\x07\n\x03ICU\x10\x01\x12\r\n\tEMERGENCY\x10\x02\x12\x0b\n\x07GENERAL\x10\x03\x12\r\n\tISOLATION\x10\x04\"a\n\x05Staff\x12\x14\n\x0ctotalDoctors\x18\x01 \x01(\r\x12\x13\n\x0btotalNurses\x18\x02 \x01(\r\x12\x19\n\x11totalSupportStaff\x18\x03 \x01(\r\x12\x12\n\notherStaff\x18\x04 \x01(\r\"\xfb\x01\n\tEquipment\x12\x0c\n\x04uuid\x18\x01 \x01(\t\x12\x36\n\x04type\x18\x02 \x01(\x0e\x32(.health_hospital_capacity.Equipment.Type\x12\x15\n\rquantityTotal\x18\x03 \x01(\r\x12\x15\n\rquantityInUse\x18\x04 \x01(\r\x12\x1d\n\x15\x65xtraQuantityRequired\x18\x05 \x01(\r\x12\x0c\n\x04tags\x18\x06 \x03(\t\"M\n\x04Type\x12\x0b\n\x07UNKNOWN\x10\x00\x12\x0b\n\x07N95MASK\x10\x01\x12\x0e\n\nVENTILATOR\x10\x02\x12\n\n\x06GLOVES\x10\x03\x12\x0f\n\x0b\x46\x41\x43\x45_SHIELD\x10\x04\x42$\n\"io.pravah.health.hospital_capacityb\x06proto3'
   ,
   dependencies=[FeedHeader__pb2.DESCRIPTOR,])
 
 
 
-_DETAIL_OWNERSHIP = _descriptor.EnumDescriptor(
+_DETAILS_OWNERSHIP = _descriptor.EnumDescriptor(
   name='Ownership',
-  full_name='health_hospital_capacity.Detail.Ownership',
+  full_name='health_hospital_capacity.Details.Ownership',
   filename=None,
   file=DESCRIPTOR,
   values=[
@@ -46,14 +46,14 @@ _DETAIL_OWNERSHIP = _descriptor.EnumDescriptor(
   ],
   containing_type=None,
   serialized_options=None,
-  serialized_start=631,
-  serialized_end=680,
+  serialized_start=636,
+  serialized_end=685,
 )
-_sym_db.RegisterEnumDescriptor(_DETAIL_OWNERSHIP)
+_sym_db.RegisterEnumDescriptor(_DETAILS_OWNERSHIP)
 
-_DETAIL_SPECIALITY = _descriptor.EnumDescriptor(
+_DETAILS_SPECIALITY = _descriptor.EnumDescriptor(
   name='Speciality',
-  full_name='health_hospital_capacity.Detail.Speciality',
+  full_name='health_hospital_capacity.Details.Speciality',
   filename=None,
   file=DESCRIPTOR,
   values=[
@@ -72,10 +72,10 @@ _DETAIL_SPECIALITY = _descriptor.EnumDescriptor(
   ],
   containing_type=None,
   serialized_options=None,
-  serialized_start=682,
-  serialized_end=733,
+  serialized_start=687,
+  serialized_end=738,
 )
-_sym_db.RegisterEnumDescriptor(_DETAIL_SPECIALITY)
+_sym_db.RegisterEnumDescriptor(_DETAILS_SPECIALITY)
 
 _WARD_TYPE = _descriptor.EnumDescriptor(
   name='Type',
@@ -106,8 +106,8 @@ _WARD_TYPE = _descriptor.EnumDescriptor(
   ],
   containing_type=None,
   serialized_options=None,
-  serialized_start=993,
-  serialized_end=1064,
+  serialized_start=1025,
+  serialized_end=1096,
 )
 _sym_db.RegisterEnumDescriptor(_WARD_TYPE)
 
@@ -140,8 +140,8 @@ _EQUIPMENT_TYPE = _descriptor.EnumDescriptor(
   ],
   containing_type=None,
   serialized_options=None,
-  serialized_start=1340,
-  serialized_end=1417,
+  serialized_start=1372,
+  serialized_end=1449,
 )
 _sym_db.RegisterEnumDescriptor(_EQUIPMENT_TYPE)
 
@@ -192,7 +192,7 @@ _HOSPITAL = _descriptor.Descriptor(
   containing_type=None,
   fields=[
     _descriptor.FieldDescriptor(
-      name='detail', full_name='health_hospital_capacity.Hospital.detail', index=0,
+      name='details', full_name='health_hospital_capacity.Hospital.details', index=0,
       number=1, type=11, cpp_type=10, label=1,
       has_default_value=False, default_value=None,
       message_type=None, enum_type=None, containing_type=None,
@@ -232,47 +232,47 @@ _HOSPITAL = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=177,
-  serialized_end=408,
+  serialized_end=410,
 )
 
 
-_DETAIL = _descriptor.Descriptor(
-  name='Detail',
-  full_name='health_hospital_capacity.Detail',
+_DETAILS = _descriptor.Descriptor(
+  name='Details',
+  full_name='health_hospital_capacity.Details',
   filename=None,
   file=DESCRIPTOR,
   containing_type=None,
   fields=[
     _descriptor.FieldDescriptor(
-      name='uuid', full_name='health_hospital_capacity.Detail.uuid', index=0,
+      name='uuid', full_name='health_hospital_capacity.Details.uuid', index=0,
       number=1, type=9, cpp_type=9, label=1,
       has_default_value=False, default_value=b"".decode('utf-8'),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR),
     _descriptor.FieldDescriptor(
-      name='name', full_name='health_hospital_capacity.Detail.name', index=1,
+      name='name', full_name='health_hospital_capacity.Details.name', index=1,
       number=2, type=9, cpp_type=9, label=1,
       has_default_value=False, default_value=b"".decode('utf-8'),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR),
     _descriptor.FieldDescriptor(
-      name='location', full_name='health_hospital_capacity.Detail.location', index=2,
+      name='location', full_name='health_hospital_capacity.Details.location', index=2,
       number=3, type=11, cpp_type=10, label=1,
       has_default_value=False, default_value=None,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR),
     _descriptor.FieldDescriptor(
-      name='ownership', full_name='health_hospital_capacity.Detail.ownership', index=3,
+      name='ownership', full_name='health_hospital_capacity.Details.ownership', index=3,
       number=4, type=14, cpp_type=8, label=1,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR),
     _descriptor.FieldDescriptor(
-      name='speciality', full_name='health_hospital_capacity.Detail.speciality', index=4,
+      name='speciality', full_name='health_hospital_capacity.Details.speciality', index=4,
       number=5, type=14, cpp_type=8, label=1,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
@@ -283,8 +283,8 @@ _DETAIL = _descriptor.Descriptor(
   ],
   nested_types=[],
   enum_types=[
-    _DETAIL_OWNERSHIP,
-    _DETAIL_SPECIALITY,
+    _DETAILS_OWNERSHIP,
+    _DETAILS_SPECIALITY,
   ],
   serialized_options=None,
   is_extendable=False,
@@ -292,8 +292,8 @@ _DETAIL = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=411,
-  serialized_end=733,
+  serialized_start=413,
+  serialized_end=738,
 )
 
 
@@ -337,8 +337,8 @@ _LOCATION = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=735,
-  serialized_end=799,
+  serialized_start=740,
+  serialized_end=804,
 )
 
 
@@ -368,8 +368,8 @@ _INFRASTRUCTURE = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=801,
-  serialized_end=864,
+  serialized_start=806,
+  serialized_end=869,
 )
 
 
@@ -408,6 +408,13 @@ _WARD = _descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='extraBedsRequired', full_name='health_hospital_capacity.Ward.extraBedsRequired', index=4,
+      number=5, type=13, cpp_type=3, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
   ],
   extensions=[
   ],
@@ -421,8 +428,8 @@ _WARD = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=867,
-  serialized_end=1064,
+  serialized_start=872,
+  serialized_end=1096,
 )
 
 
@@ -473,8 +480,8 @@ _STAFF = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=1066,
-  serialized_end=1163,
+  serialized_start=1098,
+  serialized_end=1195,
 )
 
 
@@ -540,21 +547,21 @@ _EQUIPMENT = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=1166,
-  serialized_end=1417,
+  serialized_start=1198,
+  serialized_end=1449,
 )
 
 _FEEDMESSAGE.fields_by_name['header'].message_type = FeedHeader__pb2._FEEDHEADER
 _FEEDMESSAGE.fields_by_name['hospitals'].message_type = _HOSPITAL
-_HOSPITAL.fields_by_name['detail'].message_type = _DETAIL
+_HOSPITAL.fields_by_name['details'].message_type = _DETAILS
 _HOSPITAL.fields_by_name['infrastructure'].message_type = _INFRASTRUCTURE
 _HOSPITAL.fields_by_name['staff'].message_type = _STAFF
 _HOSPITAL.fields_by_name['equipments'].message_type = _EQUIPMENT
-_DETAIL.fields_by_name['location'].message_type = _LOCATION
-_DETAIL.fields_by_name['ownership'].enum_type = _DETAIL_OWNERSHIP
-_DETAIL.fields_by_name['speciality'].enum_type = _DETAIL_SPECIALITY
-_DETAIL_OWNERSHIP.containing_type = _DETAIL
-_DETAIL_SPECIALITY.containing_type = _DETAIL
+_DETAILS.fields_by_name['location'].message_type = _LOCATION
+_DETAILS.fields_by_name['ownership'].enum_type = _DETAILS_OWNERSHIP
+_DETAILS.fields_by_name['speciality'].enum_type = _DETAILS_SPECIALITY
+_DETAILS_OWNERSHIP.containing_type = _DETAILS
+_DETAILS_SPECIALITY.containing_type = _DETAILS
 _INFRASTRUCTURE.fields_by_name['wards'].message_type = _WARD
 _WARD.fields_by_name['types'].enum_type = _WARD_TYPE
 _WARD_TYPE.containing_type = _WARD
@@ -562,7 +569,7 @@ _EQUIPMENT.fields_by_name['type'].enum_type = _EQUIPMENT_TYPE
 _EQUIPMENT_TYPE.containing_type = _EQUIPMENT
 DESCRIPTOR.message_types_by_name['FeedMessage'] = _FEEDMESSAGE
 DESCRIPTOR.message_types_by_name['Hospital'] = _HOSPITAL
-DESCRIPTOR.message_types_by_name['Detail'] = _DETAIL
+DESCRIPTOR.message_types_by_name['Details'] = _DETAILS
 DESCRIPTOR.message_types_by_name['Location'] = _LOCATION
 DESCRIPTOR.message_types_by_name['Infrastructure'] = _INFRASTRUCTURE
 DESCRIPTOR.message_types_by_name['Ward'] = _WARD
@@ -584,12 +591,12 @@ Hospital = _reflection.GeneratedProtocolMessageType('Hospital', (_message.Messag
   })
 _sym_db.RegisterMessage(Hospital)
 
-Detail = _reflection.GeneratedProtocolMessageType('Detail', (_message.Message,), {
-  'DESCRIPTOR' : _DETAIL,
+Details = _reflection.GeneratedProtocolMessageType('Details', (_message.Message,), {
+  'DESCRIPTOR' : _DETAILS,
   '__module__' : 'health.HospitalCapacity_pb2'
-  # @@protoc_insertion_point(class_scope:health_hospital_capacity.Detail)
+  # @@protoc_insertion_point(class_scope:health_hospital_capacity.Details)
   })
-_sym_db.RegisterMessage(Detail)
+_sym_db.RegisterMessage(Details)
 
 Location = _reflection.GeneratedProtocolMessageType('Location', (_message.Message,), {
   'DESCRIPTOR' : _LOCATION,
